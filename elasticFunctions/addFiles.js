c0953574d9e7fca9ec5148b2b4424521a569c656
SusmithKrishnan <tesla8.1997@gmail.com>
2021-05-26 10:04:08 +0530

    UI css and update error handling

--- elasticFunctions/addFiles.js
+++ elasticFunctions/addFiles.js
@@ -3,9 +3,6 @@ const client = require('../clients/elastic')
 module.exports = async (data) => {
 	await client.index({
 		index: 'files',
-		body: data,
-		// url: "https://kskjfbkjsd",
-		// fileName:"Addr",
-		// content: "lorem ipsum go fast "		
+		body: data
 	})
 }
--- workers/checkUpdates.js
+++ workers/checkUpdates.js
@@ -1,11 +1,13 @@
 const { redisGet, redisSet } = require('../clients/redis')
 const getUpdates = require('../dropbox/getUpdates')
+const getFilesList = require('../dropbox/getUpdates')
 const processFiles = require('./processFiles')
 
 
 module.exports = async () => {
 	var cursor = await redisGet("cursor")
 	var files = await getUpdates(cursor)
+		.catch(async(e) => { await getFilesList() })
 	await redisSet('cursor', files.cursor)
 	processFiles(files)
 }
