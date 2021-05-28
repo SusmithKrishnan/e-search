9bab452a91639ec51686b03702b979cad62653aa
SusmithKrishnan <tesla8.1997@gmail.com>
2021-05-28 10:06:03 +0530

    Elastic search ignore 404 in search, index

--- elasticFunctions/refreshIndex.js
+++ elasticFunctions/refreshIndex.js
@@ -1,5 +1,8 @@
 const client = require('../clients/elastic')
 
 module.exports = async () => {
-	await client.indices.refresh({ index: 'files' })
+	await client.indices.refresh(
+		{ index: 'files' },
+		{ ignore: [404] }
+	)
 }
--- elasticFunctions/search.js
+++ elasticFunctions/search.js
@@ -1,10 +1,13 @@
 const client = require('../clients/elastic')
 
 module.exports = async (query) => {
-	const { body } = await client.search({
-		index: 'files',
-		body: { query }
-	})
+	const { body } = await client.search(
+		{
+			index: 'files',
+			body: { query },
+		},
+		{ ignore: [404] }
+	)
 	return body.hits?.hits.map(a => {
 		return { url: a._source.url, filename: a._source.filename }
 	});
